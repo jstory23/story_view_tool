f2ad5411ad610f97189c051b9747d662d6cfd258
jstory23 <jordanstory23@gmail.com>
2017-01-23 11:38:49 -0700

    View tool initial commit

---
+++ lib/story_view_tool.rb
@@ -0,0 +1,6 @@
+require "story_view_tool/version"
+require "story_view_tool/renderer"
+
+module StoryViewTool
+  # Your code goes here...
+end
---
+++ lib/story_view_tool/version.rb
@@ -0,0 +1,3 @@
+module StoryViewTool
+  VERSION = "0.1.0"
+end
